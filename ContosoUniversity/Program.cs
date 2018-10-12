fad7e8675e42eeb6f2b15b2cc42d83231fa7ea0c
Pujan Bajra <pujanisme@gmail.com>
2018-10-12 14:39:11 +0545

    Database Setup

---
+++ ContosoUniversity/Program.cs
@@ -0,0 +1,47 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ContosoUniversity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    //context.Database.EnsureCreated();
+
+                    DbInitializer.Initialize(context);
+
+                    //Add-Migration InitialMigration
+                    //Remove-Migration
+                    //Update-Database
+                }
+                catch (Exception ex)
+
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured while creating the DB");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
